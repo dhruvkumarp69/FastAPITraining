4b49875b65ddf6132f61e68e08acd38223bc506d
dhruv <dhruvkumarp6699@gmail.com>
2026-09-21 10:26:02 +0100

    dictionary

---
+++ Day/Day1/p6.py
@@ -0,0 +1,9 @@
+#dictionary
+person = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+print(person["name"])  # Output: John
+print(person["age"])   # Output: 30
+print(person["city"])  # Output: New York   
